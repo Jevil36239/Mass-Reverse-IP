dd93f61d33e57c896339c6578b593034ed8c1c23
Jevil <51107256+Jevil36239@users.noreply.github.com>
2021-12-04 18:35:42 +0700

    Add files via upload

---
+++ lerpi.sh
@@ -0,0 +1,78 @@
+#Mass Simple Reverse IP
+#using api threatcrowd.org
+#Jevil36239
+#Finished by 04/Des/2021 18:34
+
+#modules calls
+ap=https://www.threatcrowd.org/searchApi/v2/ip/report/?ip=
+
+#Arguments awaidihduadaiwhduawdhaif
+ar(){
+read -p "Masukan List : " file;
+read -p "Threads ( Default 10 ) : " thread;
+echo ""
+}
+
+#Retarted of Boredom Section
+miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww="_"
+bwawww9wqejqeqwjeqoiwejoqwjeqjqwj="("
+zisjzojsdiaoidjqw9q9eqwojasodaoss=")"
+IAMJEVILCHASCASJDAWOAIJWDWODIJQWI="-"
+JAOWDJAIWDQw0dia0wBabysuckONcoCK1="a"
+PiiiaanwwwnggWAAABBWAAAAWAJWIEJWI="R"
+ciakkciiakkkawoohoooohqeqo2eqoek2="e"
+DUAUEEeerrBUAAHHHiiissshh22AAAAA2="s"
+ahahahhahdawhaHhahhasHQWHWHAWBDHA="M"
+RRrrarwrarwrarwreaweawerardrawdf6="r"
+bwhawbhawbdhabwhdahwbdahbwdbawdbw="I"
+ILOVEMYSELFJUSTLIKEHOWIMASTURBATE="P"
+HOlYMACRONANDCHEESEYAAAAAAAAAWAAW="v"
+#Main Page
+
+ma(){
+echo "
+          $miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$bwawww9wqejqeqwjeqoiwejoqwjeqjqwj$IAMJEVILCHASCASJDAWOAIJWDWODIJQWI $ahahahhahdawhaHhahhasHQWHWHAWBDHA$JAOWDJAIWDQw0dia0wBabysuckONcoCK1$DUAUEEeerrBUAAHHHiiissshh22AAAAA2$DUAUEEeerrBUAAHHHiiissshh22AAAAA2 $PiiiaanwwwnggWAAABBWAAAAWAJWIEJWI$ciakkciiakkkawoohoooohqeqo2eqoek2$HOlYMACRONANDCHEESEYAAAAAAAAAWAAW$ciakkciiakkkawoohoooohqeqo2eqoek2$RRrrarwrarwrarwreaweawerardrawdf6$DUAUEEeerrBUAAHHHiiissshh22AAAAA2$ciakkciiakkkawoohoooohqeqo2eqoek2 $bwhawbhawbdhabwhdahwbdahbwdbawdbw$ILOVEMYSELFJUSTLIKEHOWIMASTURBATE $IAMJEVILCHASCASJDAWOAIJWDWODIJQWI$zisjzojsdiaoidjqw9q9eqwojasodaoss$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww
+                    $miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww   
+                            $miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww$miiiiiiiiiiiiaaaaaaaaaaaawwwwwwww               
+"
+echo ""
+echo ""
+ar
+}
+
+
+
+#clearing the terminal
+nr(){
+  clear
+  ma
+}
+
+nr
+
+ #execution
+ buset (){
+ bwa=$(curl -s -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36" $ap$ip | grep -oP '"domain":"(.*?)"' | cut -d "<" -f2 | cut -d ">" -f2 | sed "s/\"//" | sed "s/domain//" | sed "s/\"//" | sed "s/\://" | sed "s/\"//" | sed "s/\"//" | uniq )
+if [[ $bwa =~ '.' ]]; then
+dapetnya=$(echo "$bwa" | wc -l)
+echo "-$2/$cekbaris- -- (-$ip-) ->  $dapetnya Sites ";
+echo "$bwa" >>  resulved.txt
+else
+echo "-$2/$cekbaris- -- (-$ip-) -> Failed ";
+fi
+ }
+
+#making default input if threads empty
+if [[ $thread="" ]]; then
+  thread=10;
+fi
+
+#wakawkaowakwoekaoa
+n=1
+IFS=$'\r\n'
+for ip in $(cat $file); do
+  f=$(expr $n % $thread)
+  cekbaris=$(cat $file | wc -l)
+  buset $ip $n &
+  n=$[$n+1]
+  done
